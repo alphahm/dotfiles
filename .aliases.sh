4aa4b88094c5e373012a38b75e9a7f4050b4f828
alphahm <ahmwebdev@gmail.com>
2024-04-14 08:40:17 +0100

    update git aliases and add after ftplugin files for json markdown and yaml

--- .aliases.sh
+++ .aliases.sh
@@ -1,11 +1,12 @@
 # git
 alias g="git"
 alias ga='git add'
-alias gc='git commit'
+alias gc='git commit -m'
 alias gc!='git commit --amend'
 alias gco='git checkout'
 alias gl='git log --graph --oneline'
 alias gpl='git pull'
+alias gplr='git pull --rebase'
 alias gph='git push'
 alias gph!='git push --force-with-lease'
 alias gss='git status -s -b'
---
+++ after/ftplugin/markdown.vim
@@ -0,0 +1,4 @@
+setlocal textwidth=80
+setlocal spell
+setlocal showbreak=…
+highlight SpellBad ctermfg=NONE ctermbg=NONE cterm=underline
